d6652b85f6df2239fcbca0cf6f3c009fd737f556
Byron <byronbl.12@gmail.com>
2019-05-03 21:49:58 +0100

    Añadimos la funcionalidad de la búsqueda. Aún sin terminar

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-$(function () {
-	console.log('jQuery is working!')
+$(document).ready(() => {
+	$('#search').keyup(() => {
+		let search = $('#search').val()
+		// Este método nos permite hacer una peticón al servidor
+		$.ajax({ // El objeto que le enviamos está compuesto por...
+			url: 'task-search.php', // Una url al archivo al que hacemos la petición
+			type: 'POST', // El tipo de método de envío
+			data: {search}, // Los datos que le pasamos
+			success: response => {console.log(response)} // La función que se ejecuta una vez llegue la respuesta del servidor
+		})
+	})
 })
